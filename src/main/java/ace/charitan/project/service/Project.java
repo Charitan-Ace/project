df2251c03c83f50347c0471ef9feb2ce11c53a7f
Ngo Quoc Binh <ngobinh0312@gmail.com>
2024-12-16 16:21:44 +0700

    finish project entity

--- src/main/java/ace/charitan/project/service/Project.java
+++ src/main/java/ace/charitan/project/service/Project.java
@@ -1,21 +1,41 @@
 package ace.charitan.project.service;
 
+import java.util.Date;
+
+import ace.charitan.project.service.ProjectEnum.StatusType;
+import ace.charitan.project.utils.AbstractEntity;
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-
-import ace.charitan.project.internal.InternalProjectDto;
-import ace.charitan.project.utils.AbstractEntity;
-import jakarta.persistence.Entity;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-class Project extends AbstractEntity  {
+class Project extends AbstractEntity {
 
-    
+    private String title;
+
+    private String description;
+
+    private double goal;
+
+    private Date startTime;
+
+    private Date endTime;
+
+    private StatusType statusType;
+
+    // private CategoryType categoryType;
+
+    // // Array of image url separated by comma
+    // private String imageUrls;
+
+    // private String thumbnailUrl ;
+
+    // private String videoUrls;
+
 }
---
+++ src/main/java/ace/charitan/project/service/ProjectEnum.java
@@ -0,0 +1,36 @@
+package ace.charitan.project.service;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import lombok.AllArgsConstructor;
+
+public class ProjectEnum {
+
+    @AllArgsConstructor
+    public static enum StatusType {
+        APPRENTICE("APPRENTICE"), ADEPT("ADEPT"), EXPERT("EXPERT");
+
+        private String value;
+
+        @JsonValue
+        public String getValue() {
+            return value;
+        }
+
+        @JsonCreator
+        public static StatusType fromValue(String value) {
+            for (StatusType status : values()) {
+                String currentStatus = status.getValue();
+                if (currentStatus.equals(value)) {
+                    return status;
+                }
+            }
+
+            // Return a response entity with a 400 Bad Request status
+            throw new IllegalArgumentException("Invalid value for StatusType Enum: " + value);
+        }
+
+    }
+
+}
