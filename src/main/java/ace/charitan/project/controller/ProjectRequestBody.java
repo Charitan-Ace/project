8f721689ac15554092c58f44cb171d48a4a79a50
Ngo Quoc Binh <ngobinh0312@gmail.com>
2024-12-16 19:34:43 +0700

    add date time validation

--- src/main/java/ace/charitan/project/controller/ProjectRequestBody.java
+++ src/main/java/ace/charitan/project/controller/ProjectRequestBody.java
@@ -2,9 +2,14 @@ package ace.charitan.project.controller;
 
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import ace.charitan.project.utils.Constants;
 
 public class ProjectRequestBody {
 
@@ -19,8 +24,14 @@ public class ProjectRequestBody {
 
         private double goal;
 
+        @DateTimeFormat(pattern = Constants.DATE_TIME_FORMAT)
+        @Pattern(regexp = Constants.DATE_TIME_REGEX, message = Constants.INVALID_DATE_TIME_MESSAGE)
+        @Future
         private Date startTime;
 
+        @DateTimeFormat(pattern = Constants.DATE_TIME_FORMAT)
+        @Pattern(regexp = Constants.DATE_TIME_REGEX, message = Constants.INVALID_DATE_TIME_MESSAGE)
+        @Future
         private Date endTime;
 
     }
