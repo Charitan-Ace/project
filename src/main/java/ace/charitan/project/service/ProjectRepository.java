12144db1f6d3e546ad3bb71e74e6d4197308feda
Ngo Quoc Binh <ngobinh0312@gmail.com>
2024-12-18 23:49:15 +0700

    update repo

--- src/main/java/ace/charitan/project/service/ProjectRepository.java
+++ src/main/java/ace/charitan/project/service/ProjectRepository.java
@@ -1,8 +1,8 @@
 package ace.charitan.project.service;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-interface ProjectRepository extends JpaRepository<Project, Long> {
+interface ProjectRepository extends MongoRepository<Project, String> {
 }
