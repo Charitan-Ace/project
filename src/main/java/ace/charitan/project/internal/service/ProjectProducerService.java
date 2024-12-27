94a78e78065795b9d3acca040be253d607aa5da5
Ngo Quoc Binh <ngobinh0312@gmail.com>
2024-12-27 13:50:42 +0700

    add service

--- src/main/java/ace/charitan/project/internal/service/ProjectProducerService.java
+++ src/main/java/ace/charitan/project/internal/service/ProjectProducerService.java
@@ -4,9 +4,11 @@ import java.io.Serializable;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 
 import ace.charitan.project.internal.dto.country.GetCountryByIsoCode.GetCountryByIsoCodeRequestDto;
 
+@Service
 class ProjectProducerService {
 
     @Autowired
