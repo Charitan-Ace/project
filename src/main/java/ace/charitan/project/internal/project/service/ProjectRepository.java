d0fe974ffe2e6061cdc378e4c7c2ed5e63b3b1f5
Ngo Quoc Binh <ngobinh0312@gmail.com>
2025-01-01 16:22:17 +0700

    test normal

--- src/main/java/ace/charitan/project/internal/project/service/ProjectRepository.java
+++ src/main/java/ace/charitan/project/internal/project/service/ProjectRepository.java
@@ -1,8 +1,13 @@
 package ace.charitan.project.internal.project.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import ace.charitan.project.internal.project.dto.project.InternalProjectDto;
+
 @Repository
 interface ProjectRepository extends JpaRepository<Project, Long> {
+    Page<InternalProjectDto> findByCountryIsoCode(String countryIsoCode, Pageable pageable);
 }
