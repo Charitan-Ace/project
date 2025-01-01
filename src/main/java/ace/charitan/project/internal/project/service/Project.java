74dce361a627a464699e2435763418fed45fc5b1
Ngo Quoc Binh <ngobinh0312@gmail.com>
2025-01-01 16:31:00 +0700

    change loggin

--- src/main/java/ace/charitan/project/internal/project/service/Project.java
+++ src/main/java/ace/charitan/project/internal/project/service/Project.java
@@ -11,12 +11,14 @@ import ace.charitan.project.internal.utils.AbstractEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+// @TableN("project")
 @Getter
 @Setter
 @NoArgsConstructor
