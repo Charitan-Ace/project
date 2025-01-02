aee8fc0085a7cf9f6d9ba6a512b28ac7b6daaaec
Ngo Quoc Binh <ngobinh0312@gmail.com>
2025-01-02 15:14:05 +0700

    ready

---
+++ src/main/java/ace/charitan/project/config/db/DatabaseSourceConfig.java
@@ -0,0 +1,18 @@
+// package ace.charitan.project.config.db;
+
+// import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+// import org.springframework.boot.context.properties.ConfigurationProperties;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+
+// @Configuration
+// class DatabaseSourceConfig {
+//     @Bean(name = "projectSource")
+//         @ConfigurationProperties("spring.datasource.todos")
+//     public DataSourceProperties todosDataSourceProperties() {
+//         return new DataSourceProperties();
+//     }
+
+
+    
+// }
