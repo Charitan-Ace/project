6802bdfd4a2fbdae35903157013f1c459cd4aaf9
Ngo Quoc Binh <ngobinh0312@gmail.com>
2025-01-02 15:42:41 +0700

    config 1 db source

--- src/main/java/ace/charitan/project/config/db/ProjectDataSourceConfig.java
+++ src/main/java/ace/charitan/project/config/db/ProjectDataSourceConfig.java
@@ -5,9 +5,11 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
 import jakarta.annotation.PostConstruct;
 
@@ -23,15 +25,25 @@ class ProjectDataSourceConfig {
     @Primary
     @Bean(name = "projectDataSource")
     @ConfigurationProperties("spring.datasource.project")
-    public DataSource projectDataSource(@Qualifier("projectDataSourceProperties") DataSourceProperties properties) {
+    DataSource projectDataSource(@Qualifier("projectDataSourceProperties") DataSourceProperties properties) {
         return properties
                 .initializeDataSourceBuilder()
                 .build();
     }
 
+    @Primary
+    @Bean(name = "projectEntityManagerFactory")
+    LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(EntityManagerFactoryBuilder builder,
+            @Qualifier("projectDataSource") DataSource dataSource) {
+
+        return builder.dataSource(dataSource)
+                .packages("ace.charitan.project.internal.project.service")
+                .persistenceUnit("project").build();
+    }
+
     @PostConstruct
     void print() {
-        System.out.println(projectDataSourceProperties().toString());
+        System.out.println("Project datsource url " + projectDataSourceProperties().toString());
     }
 
 }
--- src/main/java/ace/charitan/project/config/db/ProjectDeletedDataSourceConfig.java
+++ src/main/java/ace/charitan/project/config/db/ProjectDeletedDataSourceConfig.java
@@ -5,9 +5,10 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
 @Configuration
 public class ProjectDeletedDataSourceConfig {
@@ -19,9 +20,19 @@ public class ProjectDeletedDataSourceConfig {
 
     @Bean(name = "projectDeletedDataSource")
     @ConfigurationProperties("spring.datasource.deleted")
-     DataSource projectDeletedDataSource(@Qualifier("projectDeletedDataSourceProperties") DataSourceProperties properties) {
+    DataSource projectDeletedDataSource(
+            @Qualifier("projectDeletedDataSourceProperties") DataSourceProperties properties) {
         return properties
                 .initializeDataSourceBuilder()
                 .build();
+    }
+
+    @Bean(name = "projectDeletedEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(EntityManagerFactoryBuilder builder,
+            @Qualifier("projectDeletedDataSource") DataSource dataSource) {
+
+        return builder.dataSource(dataSource)
+                .packages("ace.charitan.project.internal.project.service")
+                .persistenceUnit("project").build();
     }
 }
