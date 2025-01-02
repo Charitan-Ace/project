efb1559878930a837502d3e8b2e1de0cc32aa8a3
Ngo Quoc Binh <ngobinh0312@gmail.com>
2025-01-02 15:18:09 +0700

    add two db source

--- src/main/java/ace/charitan/project/config/db/DatabaseSourceConfig.java
+++ src/main/java/ace/charitan/project/config/db/DatabaseSourceConfig.java
@@ -1,18 +1,21 @@
-// package ace.charitan.project.config.db;
+package ace.charitan.project.config.db;
 
-// import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-// import org.springframework.boot.context.properties.ConfigurationProperties;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-// @Configuration
-// class DatabaseSourceConfig {
-//     @Bean(name = "projectSource")
-//         @ConfigurationProperties("spring.datasource.todos")
-//     public DataSourceProperties todosDataSourceProperties() {
-//         return new DataSourceProperties();
-//     }
+@Configuration
+class DatabaseSourceConfig {
+    @Bean(name = "projectSource")
+    @ConfigurationProperties("spring.datasource.project")
+    public DataSourceProperties projectDataSourceProperties() {
+        return new DataSourceProperties();
+    }
 
-
-    
-// }
+    @Bean(name = "projectDeletedSource")
+    @ConfigurationProperties("spring.datasource.deleted")
+    public DataSourceProperties projectDeletedDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+}
