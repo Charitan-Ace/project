fa3a2c6cd01e36b78dc531c8e79b9a2b96562608
Ngo Quoc Binh <ngobinh0312@gmail.com>
2025-01-02 10:47:30 +0700

    before to db sharding

---
+++ src/main/java/ace/charitan/project/config/DatabaseSourceConfig.java
@@ -0,0 +1,12 @@
+// package ace.charitan.project.config;
+
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+
+// @Configuration
+// class DatabaseSourceConfig {
+//     @Bean(name = "projectSource")
+
+
+    
+// }
