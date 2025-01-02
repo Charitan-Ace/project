c9328c9e883402fd949c3eb8585b7cfef597a5ae
Ngo Quoc Binh <ngobinh0312@gmail.com>
2025-01-02 15:20:20 +0700

    add debug 2 db src

--- src/main/java/ace/charitan/project/config/db/DatabaseSourceConfig.java
+++ src/main/java/ace/charitan/project/config/db/DatabaseSourceConfig.java
@@ -5,17 +5,24 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import jakarta.annotation.PostConstruct;
+
 @Configuration
 class DatabaseSourceConfig {
     @Bean(name = "projectSource")
     @ConfigurationProperties("spring.datasource.project")
-    public DataSourceProperties projectDataSourceProperties() {
+   DataSourceProperties projectDataSourceProperties() {
         return new DataSourceProperties();
     }
 
     @Bean(name = "projectDeletedSource")
     @ConfigurationProperties("spring.datasource.deleted")
-    public DataSourceProperties projectDeletedDataSourceProperties() {
+    DataSourceProperties projectDeletedDataSourceProperties() {
         return new DataSourceProperties();
+    }
+
+    @PostConstruct
+    void checkDataSource() {
+        System.out.println("Project DataSource URL: " + projectDataSourceProperties().toString());
     }
 }
