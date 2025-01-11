f29f4930a906bd1fefc84ef6358d5a77e6a80906
Ngo Quoc Binh <ngobinh0312@gmail.com>
2025-01-11 15:49:56 +0700

    add code for move to deleete shard

--- src/main/java/ace/charitan/project/config/db/JdbcTemplateConfig.java
+++ src/main/java/ace/charitan/project/config/db/JdbcTemplateConfig.java
@@ -1,11 +1,15 @@
 package ace.charitan.project.config.db;
 
+import java.util.Map;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import jakarta.transaction.Transactional;
 
 @Configuration
 class JdbcTemplateConfig {
@@ -18,4 +22,6 @@ class JdbcTemplateConfig {
     public JdbcTemplate projectDeletedJdbcTemplate(@Qualifier("projectDeletedDataSource") DataSource dataSource) {
         return new JdbcTemplate(dataSource);
     }
+
+
 }
--- src/main/java/ace/charitan/project/internal/project/service/ProjectShardService.java
+++ src/main/java/ace/charitan/project/internal/project/service/ProjectShardService.java
@@ -1,9 +1,16 @@
 package ace.charitan.project.internal.project.service;
 
+import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
+
+import org.apache.kafka.common.Uuid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
 
 @Service
 class ProjectShardService {
@@ -15,4 +22,32 @@ class ProjectShardService {
     @Autowired
     @Qualifier("projectDeletedJdbcTemplate")
     private JdbcTemplate projectDeletedJdbcTemplate;
+
+    @Transactional
+    boolean moveProjectFromProjectShardToProjectDeletedShard(String id) {
+        Map<String, Object> project = projectJdbcTemplate.queryForMap("SELECT * FROM project where id = ?",
+                id);
+
+        if (Objects.isNull(project)) {
+            return false;
+        }
+
+        projectDeletedJdbcTemplate.update(
+                "INSERT INTO project (id, title, description, goal, start_time, end_time, status_type, category_type, country_iso_code, charity_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                id,
+                project.get("title"),
+                project.get("description"),
+                project.get("goal"),
+                project.get("start_time"),
+                project.get("end_time"),
+                "DELETED",
+                project.get("category_type"),
+                project.get("country_iso_code"),
+                project.get("charity_id")
+        );
+
+        projectJdbcTemplate.update("DELETE FROM project WHERE id = ?", id);
+
+        return true;
+    }
 }
