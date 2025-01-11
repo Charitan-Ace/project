5a652651e71eb527991e76deecb4e09dfcc0085e
Ngo Quoc Binh <ngobinh0312@gmail.com>
2025-01-11 11:20:52 +0700

    remove unnecessaery files

--- src/main/java/ace/charitan/project/internal/project/service/ShardByStatus.java
+++ src/main/java/ace/charitan/project/internal/project/service/ShardByStatus.java
@@ -1,11 +1,11 @@
-package ace.charitan.project.internal.project.service;
+// package ace.charitan.project.internal.project.service;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+// import java.lang.annotation.ElementType;
+// import java.lang.annotation.Retention;
+// import java.lang.annotation.RetentionPolicy;
+// import java.lang.annotation.Target;
 
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
-public @interface ShardByStatus {
-}
+// @Target(ElementType.METHOD)
+// @Retention(RetentionPolicy.RUNTIME)
+// public @interface ShardByStatus {
+// }
