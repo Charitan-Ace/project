d5e4404d31a94d9023191490778880f438f2ed95
Ngo Quoc Binh <ngobinh0312@gmail.com>
2025-01-11 11:33:16 +0700

    temp to have bug

--- src/main/java/ace/charitan/project/internal/project/service/ShardRoutingDataSource.java
+++ src/main/java/ace/charitan/project/internal/project/service/ShardRoutingDataSource.java
@@ -2,7 +2,7 @@ package ace.charitan.project.internal.project.service;
 
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
-class ShardRoutingDataSource extends AbstractRoutingDataSource {
+public class ShardRoutingDataSource extends AbstractRoutingDataSource {
 
     // private static final ThreadLocal<String> contextHolder = new ThreadLocal<>();
     // public static void setCurrentShard(String shardKey) {
